ab62cb97dd6809e682f4c9991869ec817f28d96b
Brian Maina <brayahnmainah@gmail.com>
2025-05-09 13:11:19 +0300

    dynamic page routing

---
+++ app/components/Hello.tsx
@@ -0,0 +1,11 @@
+
+"use client";
+
+const Hello = () => {
+    console.log("Hello - I am from client")
+  return (
+   <h1>Hello from Client</h1>
+  )
+}
+
+export default Hello
---
+++ app/dashboard/users/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'next/link'
+const page = () => {
+  return (
+    <div>Users
+
+<ul>
+            <li>
+                <Link href='/dashboard/users/1'> User 1 </Link> 
+                <Link href='/dashboard/users/2'> User 2 </Link> 
+                <Link href='/dashboard/users/3'> User 3 </Link> 
+                <Link href='/dashboard/users/4'> User 4 </Link> 
+                <Link href='/dashboard/users/5'> User 5 </Link> 
+                <Link href='/dashboard/users/6'> User 6 </Link> 
+                <Link href='/dashboard/users/7'> User 7 </Link> 
+            </li>
+        </ul>
+
+    </div>
+  )
+}
+
+export default page
